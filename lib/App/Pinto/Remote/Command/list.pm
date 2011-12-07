02bfdfc86a2ebe92cd063288136c700ff6846d18
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-12-07 15:11:51 -0800

    POD edits.

--- lib/App/Pinto/Remote/Command/list.pm
+++ lib/App/Pinto/Remote/Command/list.pm
@@ -81,26 +81,31 @@ None.
 Sets the format of the output using C<printf>-style placeholders.
 Valid placeholders are:
 
-  Placeholder    Meaning                     Example
-  --------------------------------------------------------------
-  %n             Package name                Foo::Bar
-  %N             Package vname               Foo::Bar-1.2
-  %v             Package version             1.2
-  %x             Index status                (* = in the index )
-  %m             Distribution maturity       (D = developer, R = release)
-  %p             Distribution index path     /A/AU/AUTHOR/Foo-Bar-1.2.tar.gz [1]
-  %P             Distribution archive path   authors\id\A\AU\AUTHOR\Foo-1.2.tar.gz [2]
-  %s             Distribution origin         (L = local, F = foreign)
-  %S             Distribution source         http://cpan.perl.org
-  %a             Distribution author         AUTHOR
-  %d             Distribution name           Foo-Bar
-  %D             Distribution vname          Foo-Bar-1.2
-  %w             Distribution version        1.2
-  %u             Distribution url            http://cpan.perl.org/authors/id/A/AU/AUTHOR/Foo-Bar-1.2.tar.gz
+  Placeholder    Meaning
+  -----------------------------------------------------------------------------
+  %n             Package name
+  %N             Package name-version
+  %v             Package version
+  %x             Index status:                   (@) = is latest
+  %y             Pin status:                     (+) = is pinned
+  %m             Distribution maturity:          (d) = developer, (r) = release
+  %p             Distribution index path [1]
+  %P             Distribution physical path [2]
+  %s             Distribution origin:            (l) = local, (f) = foreign
+  %S             Distribution source repository
+  %a             Distribution author
+  %d             Distribution name
+  %D             Distribution name-version
+  %w             Distribution version
+  %u             Distribution url
+  %%             A literal '%'
 
 
-  [1]: The index path is always a Unix-style path.
-  [2]: The archive path is always in the native style for this OS.
+  [1]: The index path is always a Unix-style path fragment, as it
+       appears in the 02packages.details.txt index file.
+
+  [2]: The physical path is always in the native style for this OS,
+       and is relative to the root directory of the repository.
 
 You can also specify the minimum field widths and left or right
 justification, using the usual notation.  For example, this is what
